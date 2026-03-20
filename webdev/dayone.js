75dbd88f79b113fa9aeba0b154a9de1d7e12bb42
Yash <yashsharma843347@gmail.com>
2026-03-20 23:23:32 +0530

    initial

---
+++ webdev/dayone.js
@@ -0,0 +1,109 @@
+// ---- 1. How to print in JavaScript ----
+
+// console.log("hello world"); // hello world print- print krne ka treeka
+// console.log(5+55); - y number add kr skte 
+// console.log("5"+ "55"); - y string add kr skte hai 
+
+
+// --------2. variables and data types in javascript -------
+// variables are Soch 👇
+// 👉 Ek dabba (box) hai
+// 👉 Usme tu kuch bhi rakh sakta hai
+
+// let dabba1 = "bmw"; // string data type
+// let dabba2 = 4000000; // number data type
+// let isAvailable = true; // boolean data type
+// let model; // undefined data type
+
+// let age = 21;
+// console.log(age);
+
+// data types are dabbba k andr kya type ki chiz hai 
+// dabba1 = "bmw" iske andr ek string hai 
+// 1. String - text data ko represent krta hai
+// 2. Number - numeric data ko represent krta hai
+// 3. Boolean - true ya false value ko represent krta hai
+// 4. Undefined - jab variable ko koi value assign nahi ki jati hai to uska data type undefined hota hai
+// // 5. Null - jab variable ko explicitly null value assign ki jati hai to uska data type null hota hai
+
+// let age = 21;
+// console.log(typeof age); // number
+//  let isLoggedIn = true ;
+
+// 📅 Day 4: Operators (Calculation + Magic ➕➖)
+// 🧠 1. Concept (simple)
+// 👉 Operators = kaam karne wale tools
+// Jaise real life me:
+// Calculator ➕➖✖️➗
+// Waise JS me:
+// 👉 Operators use hote hain calculation ke liye
+
+// let name = "yash";
+// console.log("hello" + name);
+
+// 📅 Day 5: Conditions (if–else) 
+// Soch 👇
+
+// 👉 Agar barish ho rahi hai ☔
+// → umbrella le jao
+
+// 👉 Agar nahi ho rahi
+// → mat le jao
+
+// 👉 Yehi hota hai decision
+
+// Programming me:
+// 👉 if–else = decision lene ka system
+
+
+// let raining = true;
+// let snowing = false;
+
+// if (raining){
+//     console.log("umbrella le jao");
+// }
+// else if (snowing){
+//     console.log("coat phen lo");
+// }else{
+//     console.log("aaj mausam acha hai");
+// }
+
+// let temperature = 30;
+
+// if(temperature >29){
+//     console.log("garmi hai");
+// }else if(temperature < 20){
+//     console.log("thand hai");
+// }else{
+//     console.log("mausam acha hai");
+// }
+
+
+// let loggedin = true;
+
+// if(!loggedin){
+//     console.log("logged in false hai");
+// }
+
+// else {
+//     console.log("logged in true hai");
+// }
+
+
+// 📅 Day 6: Loops (Repeat ka Power 🔁)
+
+// for(let i=10 ; i>0; i--){
+//     console.log(i);
+// }
+
+// 📅 Day 7: While Loop (jab tak condition true hai 🔁)
+
+
+
+
+
+
+
+
+
+
